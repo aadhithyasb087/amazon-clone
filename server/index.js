df669444d7bdf4d91f0349bdb3ff55602e414bf7
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 03:36:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(express.json());
 
 
-app.use(function (req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
- res.header("Access-Control-Allow-Credentials":"true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
+//  res.header("Access-Control-Allow-Credentials":"true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 // app.use(cors({origin:"https://amazon-clone-ecru-seven.vercel.app/"},method:["GET","POST"],credentials:true));
 app.use(cors())
 
