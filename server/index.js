dba5af0e2c51124616ab3dfa81f170576a449b8e
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 03:44:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,21 +7,21 @@ const app = express();
 app.use(express.json());
 
 
-// app.use(function (req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
-//  res.header("Access-Control-Allow-Credentials":"true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+ res.header("Access-Control-Allow-Origin", "https://amazon-clone-rvom.vercel.app");
+ res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
+ res.header("Access-Control-Allow-Credentials":"true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // app.use(cors({origin:"https://amazon-clone-ecru-seven.vercel.app/"},method:["GET","POST"],credentials:true));
 app.use(cors())
 
 
-const YOUR_DOMAIN = "https://amazon-clone-ecru-seven.vercel.app/";
+const YOUR_DOMAIN = "https://amazon-clone-ecru-seven.vercel.app";
 
 app.get("/", (req, res) =>
 {
