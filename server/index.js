146d209dc0737b8471987989130ac4591d85db8f
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 01:13:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://amazon-clone-ecru-seven.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
