1563348535900c1c9d3334898be85710eefc7226
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 01:40:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
  res.header("Access-Control-Allow-Origin", "https://amazon-clone-ecru-seven.vercel.app");
-res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.use(cors());
