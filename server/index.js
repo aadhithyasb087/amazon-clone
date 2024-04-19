31a6a73f9104edf7b78b939b62ff7c0556dac7a1
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 03:10:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,15 +6,16 @@ const express = require("express");
 const app = express();
 
 app.use(function (req, res, next) {
- res.header("Access-Control-Allow-Origin", "https://amazon-clone-ecru-seven.vercel.app/");
+ res.header("Access-Control-Allow-Origin", "https://amazon-clone-ecru-seven.vercel.app");
+ res.headers("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS')
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
-app.use(cors({origin:"https://amazon-clone-ecru-seven.vercel.app/"},method:["GET","POST"],credentials:true));
-// app.use(cors())
+// app.use(cors({origin:"https://amazon-clone-ecru-seven.vercel.app/"},method:["GET","POST"],credentials:true));
+app.use(cors())
 app.use(express.json());
 
 
