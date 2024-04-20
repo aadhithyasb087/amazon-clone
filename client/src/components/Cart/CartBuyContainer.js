8ddebe2c71bddaf8365c92424200acadfe6183f4
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2024-04-20 17:06:14 +0530

    Update CartBuyContainer.js

--- client/src/components/Cart/CartBuyContainer.js
+++ client/src/components/Cart/CartBuyContainer.js
@@ -26,19 +26,19 @@ function CartBuyContainer({ totalCartPrice, totalProducts }) {
     else
     {
       const stripe = await stripePromise;
-      var checkoutSession="";
-      const Server_API="https://amazon-clone-rvom.vercel.app/create-checkout-session"
-      try{
-        checkoutSession = await axios.post(Server_API,
-        {
-          cart,
-          email: user.email,
-        },
-      );
-      }
-      catch(e){
-        console.log(e);
-      }
+      // var checkoutSession="";
+      // const Server_API="https://amazon-clone-rvom.vercel.app/create-checkout-session"
+      // try{
+      //   checkoutSession = await axios.post(Server_API,
+      //   {
+      //     cart,
+      //     email: user.email,
+      //   },
+      // );
+      // }
+      // catch(e){
+      //   console.log(e);
+      // }
 
         
       // const checkoutSession = await axios.post(
@@ -48,7 +48,92 @@ function CartBuyContainer({ totalCartPrice, totalProducts }) {
       //     email: user.email,
       //   }
       // );
-     
+
+
+
+const line_items = cart.map((item) => {
+    return {
+      price_data: {
+        currency: "INR",
+        product_data: {
+          name: item.title,
+          images: [item.image],
+          description: item.description,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    };
+  });
+
+  const chekoutSession = await stripe.checkout.sessions.create({
+    shipping_address_collection: {
+      allowed_countries: ["US", "IN"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "INR",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1500,
+            currency: "INR",
+          },
+          display_name: "Next day air",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
+    phone_number_collection: {
+      enabled: true,
+    },
+    line_items: line_items,
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/checkout/success/`,
+    cancel_url: `${YOUR_DOMAIN}/checkout/cancel/`,
+    metadata: {
+      email,
+      images:JSON.stringify(cart.map(item=>{item.image}))
+    },
+  });
+  console.log(checkoutSession);
+
+
+
+      
       if (checkoutSession.data) {
         console.log(checkoutSession.data);
         const orderItems = {
